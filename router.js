e9d36e24fa1e2cfce50572c1a8bd89b6c916cc14
vivian.mzh@gmail.com <vivian.mzh@gmail.com>
2016-10-18 11:32:19 -0700

    passed response as the 3rd parameter, and call handle that has the pathname method with response

--- router.js
+++ router.js
@@ -1,10 +1,13 @@
 // making different HTTP requests point at different parts of the code(request handlers which handles different requests to different URLs differently
-function route(handle, pathname) {
+function route(handle, pathname, response) { // passed response as the 3rd parameter
   console.log('About to route a request for ' + pathname);
   if (typeof handle[pathname] === 'function') { // if we have a function/action for this pathname in handlers
-    handle[pathname](); // trigger that action
+    return handle[pathname](response); // trigger that action
   } else {
     console.log('No request handler found for ' + pathname);
+    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.write("404 Not found");
+    response.end();
   }
 }
 

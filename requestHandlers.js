7ee3a4e5953fec80b5921b674840515a03d9e61d
vivian.mzh@gmail.com <vivian.mzh@gmail.com>
2016-10-18 11:40:35 -0700

    passed response to all requestHandlers as a parameter, now whenever a requestHandler is called, it will take care of the response

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,11 +1,22 @@
-// contains placeholder function for every request handler
+// contains placeholder function for every request handler, allows us to wire the request handlers into the router, giving the router something to route to
 
-function start() {
+var exec = require("child_process").exec;
+
+function start(response) {
   console.log('Request handler "start" was called.');
+
+  exec("ls -lah", function (error, stdout, stderr) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write(stdout); // respond the result of the exec() callback function
+    response.end();
+  });
 }
 
-function upload() {
+function upload(response) {
   console.log('Request handler "upload" was called.');
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello Upload");
+  response.end();
 }
 
 exports.start = start;

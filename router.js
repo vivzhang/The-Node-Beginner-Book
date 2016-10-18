b276c5983be5de80a4e8365c96abfd5b4bb8a67c
vivian.mzh@gmail.com <vivian.mzh@gmail.com>
2016-10-18 11:05:03 -0700

    passed handle to route and call the function of the requested pathname inside handler if exists

--- router.js
+++ router.js
@@ -1,6 +1,11 @@
-// making different HTTP requests point at different parts of the code
-function route(pathname) {
+// making different HTTP requests point at different parts of the code(request handlers which handles different requests to different URLs differently
+function route(handle, pathname) {
   console.log('About to route a request for ' + pathname);
+  if (typeof handle[pathname] === 'function') { // if we have a function/action for this pathname in handlers
+    handle[pathname](); // trigger that action
+  } else {
+    console.log('No request handler found for ' + pathname);
+  }
 }
 
 exports.route = route;

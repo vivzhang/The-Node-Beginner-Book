8318322d006592b89b58c31289d9f4895b5313d5
vivian.mzh@gmail.com <vivian.mzh@gmail.com>
2016-10-18 12:14:27 -0700

    passed postData into route() and handle method

--- router.js
+++ router.js
@@ -1,8 +1,8 @@
 // making different HTTP requests point at different parts of the code(request handlers which handles different requests to different URLs differently
-function route(handle, pathname, response) { // passed response as the 3rd parameter
+function route(handle, pathname, response, postData) { // passed response as the 3rd parameter, postData as the 4th parameter
   console.log('About to route a request for ' + pathname);
   if (typeof handle[pathname] === 'function') { // if we have a function/action for this pathname in handlers
-    return handle[pathname](response); // trigger that action
+    return handle[pathname](response, postData); // trigger that action
   } else {
     console.log('No request handler found for ' + pathname);
     response.writeHead(404, {"Content-Type": "text/plain"});

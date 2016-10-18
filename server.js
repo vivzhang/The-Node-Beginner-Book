d2b1b0e2024d208574b135346fd649e2b348b1a7
vivian.mzh@gmail.com <vivian.mzh@gmail.com>
2016-10-18 11:30:56 -0700

    instead of expecting a return value from the route() and call response methods(line 10, 12, 13), pass response object to route as the 3rd arugument, so now route will take care of it

--- server.js
+++ server.js
@@ -7,15 +7,13 @@ function start(route, handle) {
     var pathname = url.parse(request.url).pathname; // the part after the '/'
     console.log('Request for ' + pathname + ' received.'); // 2. when a request is received and callback function is triggered
 
-    route(handle, pathname);
-
-    response.writeHead(200, {'Content-Type':'text/plain'}); // in HTTP response header
-    response.write('Hello World'); // in HTTP response body
-    response.end(); // finishes the response
+    // response.writeHead(200, {'Content-Type':'text/plain'}); // in HTTP response header
+    route(handle, pathname, response); // instead of expecting a return value from the route() and call response methods(line 10, 12, 13), pass response object to route as the 3rd arugument, so now route will take care of it
+    // response.write(content); // in HTTP response body
+    // response.end(); // finishes the response
   }
 
   http.createServer(onRequest).listen(8888); // get trigger every time when a new request comes in(when localhost page loads)
-
   console.log('Server has started.'); // 1. start immediately after we start the server(node server.js)
 }
 

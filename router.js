0a3bfc3d799083de3ae32bce27aacd89913d0001
vivian.mzh@gmail.com <vivian.mzh@gmail.com>
2016-10-18 10:06:59 -0700

    created route function that takes pathname as parameter

--- router.js
+++ router.js
@@ -1,1 +1,6 @@
 // making different HTTP requests point at different parts of the code
+function route(pathname) {
+  console.log('About to route a request for ' + pathname);
+}
+
+exports.route = route;

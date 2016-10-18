5d01209c59a8a56a4f9348ce008869b553eef839
vivian.mzh@gmail.com <vivian.mzh@gmail.com>
2016-10-17 21:53:20 -0700

    refactored code, saved the callback function into a variable onRequest

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 // where HTTP code lives
 var http = require('http');
 
-http.createServer(function(request, response) {
+function onRequest(request, response) {
+  console.log('Request received.');
   response.writeHead(200, {'Content-Type':'text/plain'});
   response.write('Hello World');
   response.end();
-}).listen(8888);
+}
 
+http.createServer(onRequest).listen(8888); // get trigger every time when a new request comes in
+
+console.log('Server has started.')

0769e41b06f83e7c9417c3e4527246f67535f76d
vivian.mzh@gmail.com <vivian.mzh@gmail.com>
2016-10-18 11:59:02 -0700

    added text box and submit button into the page(our POST request

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,17 +1,27 @@
 // contains placeholder function for every request handler, allows us to wire the request handlers into the router, giving the router something to route to
 
-var exec = require("child_process").exec;
-
+// by passing the response to each request handler, we will get a response as soon as that handler finish running, no need to wait till the handlers before it to finish running first(done in a non-blocking function, by using asynchronous callbacks)
 function start(response) {
   console.log('Request handler "start" was called.');
 
-  exec("ls -lah", function (error, stdout, stderr) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write(stdout); // respond the result of the exec() callback function
-    response.end();
-  });
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value"Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(body);
+  response.end();
 }
 
+// upload() will be triggered once user enter text and hit submit(send POST request), now upload will handle the POST request
 function upload(response) {
   console.log('Request handler "upload" was called.');
   response.writeHead(200, {"Content-Type": "text/plain"});
@@ -21,3 +31,4 @@ function upload(response) {
 
 exports.start = start;
 exports.upload = upload;
+

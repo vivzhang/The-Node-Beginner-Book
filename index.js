33b40409a3ef83fb75fea98a5b4ac2538c80b6c4
vivian.mzh@gmail.com <vivian.mzh@gmail.com>
2016-10-18 10:57:18 -0700

    imported requestHandlers object and added some request handlers

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // main file start the app
 var server = require('./server');
 var router = require('./router');
+var requestHandlers = require('./requestHandlers');
 
-server.start(router.route);
+var handle = {}; // a collection of request handlers
+handle['/'] = requestHandlers.start;
+handle['/start'] = requestHandlers.start;
+handle['/upload'] = requestHandlers.upload;
+
+server.start(router.route, handle);

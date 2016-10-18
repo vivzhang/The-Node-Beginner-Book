129ce915fbeddb1db04cb73cede9ac03b5e162f2
vivian.mzh@gmail.com <vivian.mzh@gmail.com>
2016-10-17 22:24:10 -0700

    created start function that responds to http requests and exported it

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 // where HTTP code lives
 var http = require('http');
 
-function onRequest(request, response) {
-  console.log('Request received.');
-  response.writeHead(200, {'Content-Type':'text/plain'});
-  response.write('Hello World');
-  response.end();
+function start() {
+  function onRequest(request, response) { // request and response are objects(contain methods to handle HTTP requests)
+    console.log('Request received.'); // 2
+    response.writeHead(200, {'Content-Type':'text/plain'}); // in HTTP response header
+    response.write('Hello World'); // in HTTP response body
+    response.end(); // finishes the response
+  }
+
+  http.createServer(onRequest).listen(8888); // get trigger every time when a new request comes in(when localhost page loads)
+
+  console.log('Server has started.'); // 1. start immediately after we start the server(node server.js)
 }
 
-http.createServer(onRequest).listen(8888); // get trigger every time when a new request comes in
-
-console.log('Server has started.')
+exports.start = start;

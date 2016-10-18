1b6ae2380b62c2038ea2a6a5c7f4878731d61ba6
vivian.mzh@gmail.com <vivian.mzh@gmail.com>
2016-10-18 10:43:29 -0700

    added requestHandlers module, created placeholder functions that handle different requests

---
+++ requestHandlers.js
@@ -0,0 +1,12 @@
+// contains placeholder function for every request handler
+
+function start() {
+  console.log('Request handler "start" was called.');
+}
+
+function upload() {
+  console.log('Request handler "upload" was called.');
+}
+
+exports.start = start;
+exports.upload = upload;
